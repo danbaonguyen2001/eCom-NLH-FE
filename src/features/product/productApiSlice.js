25a2b36a461b9a1d747ed2961ca948757e6f4258
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-12-17 16:05:00 +0700

    Last change, deploy

--- src/features/product/productApiSlice.js
+++ src/features/product/productApiSlice.js
@@ -1,49 +1,49 @@
 import { apiSlice } from "../../apis/apiSlice";
 
 export const productApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    // By Id
-    getProductById: builder.query({
-      //New
-      query: ({ productId }) => ({
-        url: `http://localhost:5000/api/products/${productId}`,
-        method: "GET",
-      }),
-    }),
+    endpoints: (builder) => ({
+        // By Id
+        getProductById: builder.query({
+            //New
+            query: ({ productId }) => ({
+                url: `https://tlcn-2022-be.onrender.com/api/products/${productId}`,
+                method: "GET",
+            }),
+        }),
 
-    // By keyword
-    getProductsList: builder.query({
-      //New
-      query: (inputData) => ({
-        url: `http://localhost:5000/api/products`,
-        params: { ...inputData },
-      }),
-    }),
+        // By keyword
+        getProductsList: builder.query({
+            //New
+            query: (inputData) => ({
+                url: `https://tlcn-2022-be.onrender.com/api/products`,
+                params: {...inputData },
+            }),
+        }),
 
-    //By Category
+        //By Category
 
-    getProductsByCategory: builder.query({
-      query: ({ categoryName }) => ({
-        url: `http://localhost:5000/api/products/category/${categoryName}`,
-        method: "GET",
-      }),
-    }),
+        getProductsByCategory: builder.query({
+            query: ({ categoryName }) => ({
+                url: `https://tlcn-2022-be.onrender.com/api/products/category/${categoryName}`,
+                method: "GET",
+            }),
+        }),
 
-    // Top Products
-    getProductsTop: builder.query({
-      query: () => ({
-        url: `http://localhost:5000/api/products/topreviews`,
-      }),
-    }),
+        // Top Products
+        getProductsTop: builder.query({
+            query: () => ({
+                url: `https://tlcn-2022-be.onrender.com/api/products/topreviews`,
+            }),
+        }),
 
-    // All products
-    getAllProducts: builder.query({
-      // common key : keyword
-      // {manufacturerId,categoryId,subCategoryId,page,size}
-      //
-      query: () => ({
-        url: `http://localhost:5000/api/products`,
-      }),
+        // All products
+        getAllProducts: builder.query({
+            // common key : keyword
+            // {manufacturerId,categoryId,subCategoryId,page,size}
+            //
+            query: () => ({
+                url: `https://tlcn-2022-be.onrender.com/api/products`,
+            }),
+        }),
     }),
-  }),
 });
