526f2fe3d584de3fb3a94c0ea1ac05eedb1207ae
Loc Le Hoang <19110237@student.hcmute.edu.vn>
2022-12-22 16:06:20 +0700

    Change newnear theme

--- src/components/home/BigBanner.jsx
+++ src/components/home/BigBanner.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "../../assets/css/home/bigbanner.css";
+import "../../sass/home/_bigbanner.scss";
 // import bigBannerImg from "../../assets/images/home/bigBanner.png";
 
 import { hoData } from "./mockData";
 const Slider = React.lazy(() => import("./subComponent/Slider"));
 // mock data
-const dataS = hoData
+const dataS = hoData;
 const BigBanner = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -14,7 +14,7 @@ const BigBanner = () => {
   }, [data]);
   // slider config
   const settings = {
-    navArr:true,
+    navArr: true,
     infinite: true,
     speed: 500,
     slidesToShow: 2,
@@ -24,10 +24,14 @@ const BigBanner = () => {
   return (
     <div className="bigBannerWrap">
       <div className="bigBanner__img">
-        <LazyLoadImage src={"//cdn.tgdd.vn/2022/08/banner/1234787232-1920x450.png"} />
+        <LazyLoadImage
+          src={
+            "https://cdn.tgdd.vn/2022/12/banner/Banner-Big-Desk-min-1920x450.webp"
+          }
+        />
       </div>
       <div className="bigBanner__slider">
-        <Slider  data={data} settings={settings} />
+        <Slider data={data} settings={settings} />
       </div>
     </div>
   );
